17ba43a5f5d49592142449dcd9414837b01e7c3f
JoacoCinnamon <joacopontello2002@gmail.com>
2024-10-03 21:58:54 -0300

    Add initial Steam consts

---
+++ steam/consts.ts
@@ -0,0 +1,3 @@
+export const STORE_STEAMPOWERED_URL = "https://store.steampowered.com";
+export const STORE_STEAMPOWERED_API_URL = "https://store.steampowered.com/api";
+export const I_HAVE_NO_MOUTH_AND_I_MUST_SCREAM_ID = 245390;
